8804f0a25557127c0f29119abb7eb0b4596609c7
ScaY <stephane.viper@gmail.com>
2015-02-16 16:12:37 +0100

    Maj position marker

--- app/src/main/java/fr/isen/shazamphoto/ui/NearestMonuments.java
+++ app/src/main/java/fr/isen/shazamphoto/ui/NearestMonuments.java
@@ -71,6 +71,6 @@ public class NearestMonuments extends FragmentActivity {
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(50.633885, 3.048681)).title("Marker"));
     }
 }
