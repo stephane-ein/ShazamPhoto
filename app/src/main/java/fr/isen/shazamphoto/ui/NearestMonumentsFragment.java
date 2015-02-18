565d003d97e3efd1a54da89393b2e9509ecdf5b6
ScaY <stephane.viper@gmail.com>
2015-02-18 12:54:06 +0100

    Maj bugs (Manifest)

--- app/src/main/java/fr/isen/shazamphoto/ui/NearestMonumentsFragment.java
+++ app/src/main/java/fr/isen/shazamphoto/ui/NearestMonumentsFragment.java
@@ -7,8 +7,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import fr.isen.shazamphoto.R;
+import fr.isen.shazamphoto.database.Monument;
+import fr.isen.shazamphoto.database.TaggedMonumentDAO;
 
 public class NearestMonumentsFragment extends Fragment {
 
@@ -32,14 +38,29 @@ public class NearestMonumentsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_nearest_monuments, container, false);
-        Button button = (Button) view.findViewById(R.id.button_launch_google_map);
+      /*  Button button = (Button) view.findViewById(R.id.button_launch_google_map);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getActivity(), NearestMonuments.class);
                 startActivity(intent);
             }
-        });
+        });*/
+
+        ListView listview = (ListView) view.findViewById(R.id.listview_nearest_monument);
+
+        TaggedMonumentDAO taggedMonumentDAO = new TaggedMonumentDAO(getActivity());
+        taggedMonumentDAO.open();
+        final List<Monument> monumentsList = taggedMonumentDAO.getAllMonuments();
+        taggedMonumentDAO.close();
+        final ArrayList<Monument> monuments = new ArrayList<>();
+        for (Monument monument : monumentsList) {
+            monuments.add(monument);
+        }
+
+        CustomListAdapter adapter = new CustomListAdapter(getActivity(), monuments);
+        listview.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
         return view;
     }
 
--- app/src/main/java/fr/isen/shazamphoto/ui/TaggedMonument.java
+++ app/src/main/java/fr/isen/shazamphoto/ui/TaggedMonument.java
@@ -10,59 +10,5 @@ public class TaggedMonument extends MonumentList {
     public TaggedMonument() {
         super(TaggedMonument.class.getSimpleName());
     }
-/*
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_tagged_monument, container, false);
-
-        FavouriteMonumentDAO favouriteMonumentDAO = new FavouriteMonumentDAO(getActivity());
-        favouriteMonumentDAO.open();
-        final List<Monument> monumentsList = favouriteMonumentDAO.getAllMonuments();
-        favouriteMonumentDAO.close();
-        final ArrayList<Monument> monuments = new ArrayList<Monument>();
-        for (Monument monument : monumentsList) {
-            monuments.add(monument);
-        }
-
-        CustomListAdapter adapter = new CustomListAdapter(getActivity(), monuments);
-        ListView listview = (ListView) view.findViewById(R.id.listview_favourite_monument);
-        listview.setAdapter(adapter);
-        adapter.notifyDataSetChanged();
-
-        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> arg0, View arg1,
-                                    int position, long arg3) {
-
-                Intent intent = new Intent(getActivity(), DetailMonument.class);
-
-                intent.putExtra(Monument.NAME_SERIALIZABLE, monuments.get(position));
-                startActivity(intent);
-            }
-        });
-        setRetainInstance(true);
-
-        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> arg0, View arg1,
-                                    int position, long arg3) {
-
-                Intent intent = new Intent(getActivity(), DetailMonument.class);
-
-                intent.putExtra(Monument.NAME_SERIALIZABLE, monuments.get(position));
-                startActivity(intent);
-
-            }
-        });
-        setRetainInstance(true);
-        return view;
-    }
-*/
 }
